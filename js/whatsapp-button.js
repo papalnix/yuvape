194920dd937bdaef18d0b5170500ed97cfd5a748
darian <darianzheng@163.com>
2025-06-03 22:33:43 +0800

    号码地区错了 是852

--- js/whatsapp-button.js
+++ js/whatsapp-button.js
@@ -7,7 +7,7 @@ function initWhatsAppButton() {
     }
 
     // 随便写个号码先
-    const phoneNumber = '8595849018'; // 香港號碼
+    const phoneNumber = '85295849018'; // 香港號碼
     const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
     
     // WhatsApp SVG 图标
